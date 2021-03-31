a19e85ba218d3aa6b72cb8235d8c13e08f219f78
carigba <carigba92@gmail.com>
2021-03-31 18:02:03 +0200

    h

--- test.py
+++ test.py
@@ -2,11 +2,8 @@ import os
 
 print('Hello world, I am Carles')
 
-<<<<<<< HEAD
 members = ['Luis','cARLES','CASTILLO']
 
 for i, member in members:
     print('Member {}: {}'.format(i,member))
-=======
 print('Vamos verdu arreglame la GPU')
->>>>>>> e5796ecd5945cd9d13ca2a3bff88b067a90aa417
