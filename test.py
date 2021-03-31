776c6e5a1fdbdba88965f2e9d7304dec1aa7235d
Daniel Verdu <davertor@gmail.com>
2021-03-31 18:03:29 +0200

    new_members added

--- test.py
+++ test.py
@@ -2,11 +2,9 @@ import os
 
 print('Hello world, I am Carles')
 
-<<<<<<< HEAD
-members = ['Luis','cARLES','CASTILLO']
+members = ['Luis','cARLES','CASTILLO','Paula','Dani','Alvaro']
 
 for i, member in members:
     print('Member {}: {}'.format(i,member))
-=======
-print('Vamos verdu arreglame la GPU')
->>>>>>> e5796ecd5945cd9d13ca2a3bff88b067a90aa417
+
+print('GPU arreglada')
